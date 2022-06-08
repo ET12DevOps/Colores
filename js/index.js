258050547476080289361a88637e64f552a9e054
jvelazquez <jonathanvgms@gmail.com>
2022-06-08 10:31:23 -0300

    Add dinamic html

--- js/index.js
+++ js/index.js
@@ -15,17 +15,54 @@ document.addEventListener('DOMContentLoaded', () => {
         email: 'voy@rtucasa.com'
     }
 
-    let jose = {
-        nombre: 'jose',
-        apellido: 'perez',
-        edad: 20,
-        email: 'algo@mail.com'
-    }
-
     let alumnos = []
 
     alumnos.push(juan)
-    alumnos.push(jose)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
+    alumnos.push(juan)
 
     console.log(alumnos)
+
+    // let alumno1 = document.getElementById('alumno1')
+    // alumno1.innerHTML = alumnos[0].nombre
+
+    // let alumno2 = document.getElementById('alumno2')
+    // alumno2.innerHTML = alumnos[1].nombre
+    let clase = document.getElementById('clase')
+    let tarjetas = '<div class="columns">'
+
+    for (var i = 0; i < alumnos.length; i++) {
+        //crear una tarjeta para cada alumno con el nombre
+        tarjetas += `
+        <div class="column is-one-fifth">
+            <div class="box">
+                ${alumnos[i].nombre + i}
+            </div>
+        </div>
+        `
+        if ((i + 1) % 5 == 0 && i > 0) {
+            tarjetas += `
+            </div> 
+            <div class="columns">
+            `
+        }
+    }
+
+    tarjetas += `</div>`
+
+    console.log(tarjetas)
+    clase.innerHTML = tarjetas
 })
