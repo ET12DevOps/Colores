385051d53d7db8ccbaa052ad2d522c5805a2ac2e
jvelazquez <jonathanvgms@gmail.com>
2022-06-08 08:34:55 -0300

    definiciones js

--- js/index.js
+++ js/index.js
@@ -1,39 +1,59 @@
-document.addEventListener('DOMContentLoaded', cargaInicial)
+//variables
+const variableConstante = 0.32
+let variableConAlcance = 'cadena de caracteres'
+var variableSinAlcance = false
 
-function cargaInicial() {
-    let colores = [] //lista de objetos con los colores
+//variable entera
+let numero = 0
+console.log(numero)
+numero++
+console.log(numero)
 
-    //creo objeto color rojo
-    let rojo = {
-        name: 'red',
-        hex: '#ff0000'
-    }
+//listas
+let numeros = [2, 3, 4, 5, 6, 7]
+let palabras = ['dos', 'tres', 'cuatro', 'cinco']
+let algo = ['dos', 3, 0.4, true]
+console.log(numeros)
+console.log(palabras)
+console.log(algo)
 
-    //creo objeto color azul
-    let azul = {
-        name: 'blue',
-        hex: '#0000ff'
-    }
-
-    //creo objeto color verde
-    let verde = {
-        name: 'green',
-        hex: '#00ff00'
-    }
-
-    //agrego los objetos color a la lista de colores
-    colores.push(rojo)
-    colores.push(verde)
-    colores.push(azul)
-
-    let divColores = document.getElementById('colores')
-    let tarjetasColores = ''
-
-    for (let i = 0; i < colores.length; i++) {
-        tarjetasColores += `<p>${colores[i].name}</p>`
-    }
-
-    console.log(tarjetasColores)
-
-    divColores.innerHTML = tarjetasColores
+//objetos
+let juan = {
+    nombre: 'juan',
+    apellido: 'perez',
+    edad: 19,
+    email: 'voy@rtucasa.com'
 }
+
+//console.log(juan)
+
+let jose = {
+    nombre: 'jose',
+    apellido: 'perez',
+    edad: 20,
+    email: 'algo@mail.com'
+}
+//console.log(jose)
+
+let alumnos = []
+
+alumnos.push(juan) //agregar un objeto al vector
+alumnos.push(jose) //agregar un objeto al vector
+console.log(alumnos)
+
+//funciones
+//defino la funcion
+function imprimirAlumnos() {
+    //cuerpo de la funcion
+    for (var i = 0; i < alumnos.length; i++) {
+        console.log(alumnos[i])
+    }
+}
+
+//realiza el llamado a la funcion para su ejecucion
+imprimirAlumnos()
+
+//funcion anonima
+// () => {
+    //cuerpo de la funcion
+// }
